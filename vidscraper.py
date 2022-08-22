ae707d35f8fb36f99aee0172c2c62dc68a00ae5e
Amoghraj <amogh@ajnalens.com>
2022-08-22 18:35:30 +0530

    keyword named Folder based storage of videos

--- vidscraper.py
+++ vidscraper.py
@@ -1,3 +1,5 @@
+import csv
+import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,54 +9,79 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from pytube import YouTube
 
-def down(link):
+def down(link,storedir):
     yt = YouTube(link)
     print("Title: ",yt.title)
     print("Number of views: ",yt.views)
     print("Length of video: ",yt.length)
     ys = yt.streams.get_highest_resolution()
     print("Downloading...")
-    ys.download()
+    ys.download(storedir)
     print("Download completed!!")
 
 
-searchTerm = input("Enter Your Search Term : ")
-noOfVideos= int(input("Enter Number of Videos you want to Download : "))
 
+def getUrls(searchTerm):
 
-options = webdriver.ChromeOptions()
-#options.add_argument(r"--user-data-dir=C:\Users\amogh\AppData\Local\Google\Chrome\User Data")
-#options.add_argument(r'--profile-directory=Profile 2')
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    noOfVideos=3 #Set this to the number of videos you want to download
 
+    if searchTerm in os.listdir():
+        ae=True
+        print(searchTerm+" already exists.")
+    else:
+        os.mkdir(searchTerm)
 
+    
 
-
-driver.get("https://www.youtube.com/results?search_query="+searchTerm+"&sp=EgIYAw%253D%253D")
-
-user_data = driver.find_elements(
-        by=By.XPATH,
-        value='//*[@id="video-title"]'
-    )
-
-links = []
-for i in user_data:
-            links.append(i.get_attribute('href'))
-
-totalLinks=len(links)
-
-rl=totalLinks-noOfVideos
-
-for i in links:
-    if i=="None":
-        links.remove(i)
-
-del links[-rl:]
+    options = webdriver.ChromeOptions()
+    #options.add_argument(r"--user-data-dir=C:\Users\amogh\AppData\Local\Google\Chrome\User Data")
+    #options.add_argument(r'--profile-directory=Profile 2')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 
 
-for i in links:
-    print(i)
-    down(i)
+
+    driver.get("https://www.youtube.com/results?search_query="+searchTerm+"&sp=EgIYAw%253D%253D")
+
+    user_data = driver.find_elements(
+            by=By.XPATH,
+            value='//*[@id="video-title"]'
+        )
+
+    links = []
+    for i in user_data:
+                links.append(i.get_attribute('href'))
+
+    totalLinks=len(links)
+
+    rl=totalLinks-noOfVideos
+
+
+    print(links)
+    nl=[]
+    for i in links:
+        if i==None:
+            links.remove(i)
+        else:
+            nl.append(i)
+    print(nl)
+    del nl[-rl:]
+
+
+    cwd=os.getcwd()+"/"
+    
+    storedir=cwd+searchTerm+"/"
+
+    for i in nl:
+        print(i)
+        down(i,storedir)
+
+    sleep(2)
+
+
+with open('keywords.csv') as file_obj:
+    reader_obj = csv.reader(file_obj)
+    for row in reader_obj:
+        getUrls(row[0])
 
 sleep(2)
